ac8b5b55bcbcc6b02b7addf64c8949f3a2fee56e
Bruce S <rainbow.initialize@gmail.com>
2022-12-03 00:31:28 +0700

    update monitor prover

--- monitor_aleo.sh
+++ monitor_aleo.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
 client_proc=$(ps -ef | grep snarkos | grep client | wc -l)
+prover_proc=$(ps -ef | grep snarkos | grep prover | wc -l)
 if [ "x$client_proc" != "x1" ]; then systemctl restart aleo-client; fi
+if [ "x$prover_proc" != "x1" ]; then systemctl restart aleo-prover; fi
